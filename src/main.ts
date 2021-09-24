b92e8f2c65264b2ca93890323bd11686652e3139
Greg Zaitsev <greg.zaitsev@gmail.com>
2021-09-24 15:22:05 +0300

    disable cors

--- src/main.ts
+++ src/main.ts
@@ -33,7 +33,7 @@ function ignoreQueryCase(app: INestApplication) {
   });}
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: false });
 
   useSwagger(app);
   ignoreQueryCase(app);
